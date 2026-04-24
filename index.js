5ddf653063cf31836108c1991269ac62bccf6bac
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-04-24 03:24:08 +0000

    Complete the ui for the site

---
+++ index.js
@@ -0,0 +1,14 @@
+const viewPortHeight = document.documentElement.clientHeight
+const viewPortWidth = document.documentElement.clientWidth
+
+const textContainer = document.querySelector(".main")
+const main = document.querySelector("main")
+
+// now when text content is more than 100vh instead of increasing in height and causing 
+// a layout break the flex direction will set to column
+
+if (textContainer.offsetHeight > viewPortHeight || viewPortWidth < 950){
+    main.style.flexDirection = "column"
+    const imgContainer = document.querySelector(".img-logo-con")
+    imgContainer.classList.add("img-logo-con-colum-styles")
+}
