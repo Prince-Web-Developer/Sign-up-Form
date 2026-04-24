a726d46cbcf802ee618df21015d99d2f00ac9a72
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-04-24 05:36:24 +0000

    Validate password and confirm password via js
    - users now need to type both password and confirm password same

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const main = document.querySelector("main")
 // now when text content is more than 100vh instead of increasing in height and causing 
 // a layout break the flex direction will set to column
 
-if (textContainer.offsetHeight > viewPortHeight || viewPortWidth < 950){
+if (textContainer.offsetHeight > viewPortHeight || viewPortWidth < 950) {
     main.style.flexDirection = "column"
     const imgContainer = document.querySelector(".img-logo-con")
     imgContainer.classList.add("img-logo-con-colum-styles")
@@ -16,6 +16,37 @@ if (textContainer.offsetHeight > viewPortHeight || viewPortWidth < 950){
 
 const form = document.querySelector("form")
 
-form.addEventListener("submit",(e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
+
+    
+
+
+    const data = new FormData(e.target);
+    const values = Object.fromEntries(data.entries());
+
+
+    if (values["Cpassword"] != values["password"]) {
+        addClass("invalid")
+        changeFormMeassage("Both Password and Confirm Password Should be same")
+    }else {
+        addClass("opion")
+        changeFormMeassage("")
+    }    
 })
+
+
+
+
+function changeFormMeassage(msg) {
+    const meassageContainer = document.querySelector(".message")
+    meassageContainer.innerText = msg
+}
+
+function addClass(cl) {
+    const passSpan = document.querySelectorAll("#passValid")
+    passSpan.forEach((e) => {
+        e.className = ""
+        e.classList.add(cl)
+    })
+}
